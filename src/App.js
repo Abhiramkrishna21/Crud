ec3041bad089144a2b9125a6b876992a96f38004
Abhiram krishna21 <abhiramkrishna328@gmail.com>
2023-11-15 18:21:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Company from './Component/Company';
+import { Provider } from 'react-redux';
+import compstore from './Redux/Store';
+import { ToastContainer } from 'react-toastify';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={compstore}>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Company></Company>}></Route>
+        </Routes>
+        </BrowserRouter>
+        <ToastContainer position='top-left'></ToastContainer>
+        </Provider>
+    
     </div>
   );
 }
